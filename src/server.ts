5d7b539995dbfd61419c26cb200c2464a280df2b
Wr3tchedTorch <ericmoura511@gmail.com>
2025-03-25 20:11:54 -0300

    update port assignment logic

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata';
 import app from "./app";
 import env from "./environment/env";
 
-app.listen(env.APP_PORT, () => {
-  console.log(`🚀 Servidor rodando: http://localhost:${env.APP_PORT}\n`);
+const port = env.APP_PORT ? env.APP_PORT : 5173;
+
+app.listen(port, () => {
+  console.log(`🚀 Servidor rodando: http://localhost:${port}\n`);
 });
