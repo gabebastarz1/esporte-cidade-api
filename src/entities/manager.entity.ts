26b793abab97d0317852d40b56da914f87650a14
Eric Moura <124516716+Wr3tchedTorch@users.noreply.github.com>
2025-06-06 12:50:32 -0300

    add manager password-reset

--- src/entities/manager.entity.ts
+++ src/entities/manager.entity.ts
@@ -1,5 +1,10 @@
-import { Entity } from "typeorm";
+import { Entity, OneToMany } from "typeorm";
 import { UserBase } from "./user-base.entity";
+import { Token } from "./token.entity";
 
 @Entity('manager')
-export class Manager extends UserBase{ }
+export class Manager extends UserBase 
+{
+    @OneToMany(() => Token, (t) => t.manager)
+    tokens: Token[];
+}
--- src/entities/token.entity.ts
+++ src/entities/token.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm"
 import { Teacher } from "./teacher.entity"
+import { Manager } from "./manager.entity"
 
 @Entity("token")
 export class Token {
@@ -11,6 +12,9 @@ export class Token {
     
     @ManyToOne(() => Teacher, (teacher) => teacher.tokens)
     teacher: Teacher
+
+    @ManyToOne(() => Manager, (manager) => manager.tokens)
+    manager: Manager
 
     @Column({type: "datetime", default: () => "CURRENT_TIMESTAMP"})
     createdAt: string
